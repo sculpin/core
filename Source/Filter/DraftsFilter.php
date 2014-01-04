08766226b35a9668a6309b5813bae3736271e75f
Beau Simensen <beau@dflydev.com>
2014-01-03 23:38:14 -0600

    Migrate from Posts Bundle to Content Types bundle
    refs #95

---
+++ Source/Filter/DraftsFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sculpin\Core\Source\Filter;
+
+use Sculpin\Core\Source\Filter\FilterInterface;
+use Sculpin\Core\Source\SourceInterface;
+use Sculpin\Core\Util\DirectorySeparatorNormalizer;
+
+class DraftsFilter implements FilterInterface
+{
+    private $publishDrafts;
+
+    public function __construct($publishDrafts = false) {
+        $this->publishDrafts = $publishDrafts;
+    }
+
+    public function match(SourceInterface $source)
+    {
+        if ($source->data()->get('draft')) {
+            if (!$this->publishDrafts) {
+                // If we are not configured to publish drafts we should
+                // inform the source that it should be skipped. This
+                // will ensure that it won't be touched by any other
+                // part of the system for this run.
+                $source->setShouldBeSkipped();
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
